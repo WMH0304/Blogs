caaef0b3a15af5aaf5038bbdee5e639419b18334
TK <948370689@qq.com>
2021-08-30 11:21:03 +0800

    项目创建

---
+++ Blogs.DataModels/SqlDataModels/Bg_article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.DataModels.SqlDataModels
+{
+
+    /// <summary>
+    /// 文章
+    /// </summary>
+    class Bg_article
+    {
+    }
+}
---
+++ Blogs.DataModels/SqlDataModels/Bg_user.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.DataModels.SqlDataModels
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    class Bg_user
+    {
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string account { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string passwordd { get; set; }
+
+
+    }
+}
